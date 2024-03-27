279230d3dc56a1e5f32e29d60a180b70ed072f92
Dida <>
2024-03-27 13:39:25 +0400

    Add hw5

---
+++ Js/HW5.js
@@ -0,0 +1,79 @@
+"use strict";
+
+/* 1
+Дан объект numbers. 
+Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
+*/
+
+const numbers = {
+  key1: 12,
+  key2: 2,
+  key3: 4,
+  key4: 1,
+  key5: -244,
+  key6: 0,
+  key7: 7,
+};
+for (const key in numbers) {
+  if (numbers[key] >= 3) {
+    console.log(numbers[key]);
+  }
+}
+/* 2
+Необходимо из объекта post, вывести значения, к которым приписан комментарий, 
+в консоль.
+*/
+
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
+/*3
+Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя
+метод forEach.*/
+
+const products = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
+
+products.forEach((product) => {
+  product.price *= 0.85;
+});
+console.log(products);
