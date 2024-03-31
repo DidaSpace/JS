eb98c8290ce90845711ef28ef55a1b85cdc3eea6
Dida <>
2024-03-31 15:51:01 +0400

    Add hw5

--- Js/HW5.js
+++ Js/HW5.js
@@ -77,3 +77,67 @@ products.forEach((product) => {
   product.price *= 0.85;
 });
 console.log(products);
+/* 4
+1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна
+фотография, используя метод filter. Исходные данные - массив products.
+2. Необходимо отсортировать массив products используя метод sort по цене,
+начиная с самой маленькой, заканчивая самой большой ценой, после чего вывести
+отсортированный массив в консоль.
+Если сложно работать с методами массива, то можно сделать и обычным циклом.
+*/
+
+const products = [
+  {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg", "2.jpg"],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg"],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+const filterProducts = products.filter(
+  (product) => product.photos && product.photos.length > 0
+);
+console.log(filterProducts);
+
+products.sort((a, b) => a.price - b.price);
+console.log(products);
+
+("use strict");
+
+/* 5
+Необходимо создать объект, в котором будут объединены два массива en и ru 
+так, чтобы в объекте ключами выступали значения из массива en, а значениями 
+объекта являлись значения из массива ru.
+*/
+
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = [
+  "понедельник",
+  "вторник",
+  "среда",
+  "четверг",
+  "пятница",
+  "суббота",
+  "воскресенье",
+];
+
+const enRu = {};
+
+for (let i = 0; i < 7; i++) {
+  enRu[en[i]] = ru[i];
+}
+console.log(enRu);
